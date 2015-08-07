8fbec6a441f9e04a50729587b7b34eb7b3dad842
tizot <masset.camille@gmail.com>
2015-08-07 17:19:00 +0200

    Unregister Group in admin site

--- xpassion_mag/admin.py
+++ xpassion_mag/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from xpassion_mag.models import Article, Feature, Issue, Theme, Category
+
+admin.site.unregister(Group)
 
 admin.site.register(Article)
 admin.site.register(Feature)
